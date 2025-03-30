a0ad9d887a0b88d567d68be08dbdf2bb83346e7c
Miguel Muller <miguelsmuller@users.noreply.github.com>
2025-03-30 19:40:57 -0300

    feat: initilize database

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,15 @@
-from re import A
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from src.adapters.input.router import router
+from src.config.database import init_database
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_database()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
 
