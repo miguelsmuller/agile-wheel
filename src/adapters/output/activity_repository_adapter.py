372942eb87b3e940b826b3729e1e09dd54d2febe
Miguel Muller <miguelsmuller@users.noreply.github.com>
2025-04-06 20:20:49 -0300

    feat: add find_one method to ActivityRepositoryAdapter and ActivityRepositoryPort

--- src/adapters/output/activity_repository_adapter.py
+++ src/adapters/output/activity_repository_adapter.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from src.adapters.output.activity_document_mongo import ActivityDocument
 from src.application.ports.output.activity_repository import ActivityRepositoryPort
 from src.domain.entities.activity import Activity
@@ -23,6 +25,15 @@ class ActivityRepositoryAdapter(ActivityRepositoryPort):
             await activity_document.insert()
 
         return activity_document.to_domain()
+
+    async def find_one(self, activity_id: UUID) -> Activity | None:
+        activity_document = await ActivityDocument.find_one(
+            ActivityDocument.app_id == str(activity_id)
+        )
+
+        if activity_document:
+            return activity_document.to_domain()
+        return None
 
     async def find_all(self):
         docs = await ActivityDocument.find_all().to_list()
--- src/application/ports/output/activity_repository.py
+++ src/application/ports/output/activity_repository.py
@@ -1,5 +1,5 @@
-# src/ports/output/user_repository.py
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 from src.domain.entities.activity import Activity
 
@@ -10,6 +10,9 @@ class ActivityRepositoryPort(ABC):
 
     @abstractmethod
     async def update(self, user: Activity) -> Activity: ...
+
+    @abstractmethod
+    async def find_one(self, activity_id: UUID) -> Activity | None: ...
 
     @abstractmethod
     async def find_all(self) -> list[Activity]: ...
