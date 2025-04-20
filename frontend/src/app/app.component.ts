cb86e939d12cbae8dfe21be6583072734a407a55
Miguel Muller <miguelsmuller@users.noreply.github.com>
2025-04-19 23:21:33 -0300

    feat: add floating action button with menu for navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterModule } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'front';
-}
+export class AppComponent {}
