d5a7c1560b19e7da0fb8314565579c286b6004e7
Miguel Muller <miguelsmuller@users.noreply.github.com>
2025-04-27 07:50:20 -0300

    feat: update Docker configurations for backend and frontend

--- backend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Define o comando padrão
-DEFAULT_CMD="poetry run uvicorn src.main:app --host 0.0.0.0 --port ${INTERNAL_PORT}"
-
-# Executa o comando padrão com quaisquer argumentos adicionais passados
-exec $DEFAULT_CMD "$@"
--- frontend/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-envsubst '${INTERNAL_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-exec /docker-entrypoint.sh \
-    nginx -g "daemon off;"
